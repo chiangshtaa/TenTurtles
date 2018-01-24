068295191b61c8a170c5674f8839036d22755a5f
Michael Chiang <chiangshtaa@gmail.com>
2018-01-23 16:14:44 -0800

    minor fix

--- webpack.config.js
+++ webpack.config.js
@@ -19,33 +19,35 @@ module.exports = {
         }
       },
       {
-    test: /\.(gif|png|jpe?g|svg)$/i,
-    loaders: [
-      'file-loader', {
-        loader: 'image-webpack-loader',
-        options: {
-          gifsicle: {
-            interlaced: false,
-          },
-          optipng: {
-            optimizationLevel: 7,
-          },
-          pngquant: {
-            quality: '65-90',
-            speed: 4
-          },
-          mozjpeg: {
-            progressive: true,
-            quality: 65
-          },
-          // Specifying webp here will create a WEBP version of your JPG/PNG images
-          webp: {
-            quality: 75
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        loaders: [
+          'file-loader', {
+            loader: 'image-webpack-loader',
+            options: {
+              gifsicle: {
+                interlaced: false,
+              },
+              optipng: {
+                optimizationLevel: 7,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // Specifying webp here will create a WEBP version of your JPG/PNG images
+              webp: {
+                quality: 75
+              }
+            }
           }
-        }
+        ],
+        exclude: /node_modules/,
+        include: __dirname
       }
-    ]
-  }
       // {
       //   test: /\.(jpe?g|png|gif|svg)$/i,
         // loaders: [
